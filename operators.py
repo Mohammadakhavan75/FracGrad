54a1573b5664886ca101e602167c24e777bdaa3a
asalmohammadjafari <1382asal@gmail.com>
2024-08-15 11:19:01 +0330

    Verification of formula for Frac and multiFrac

--- operators.py
+++ operators.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 
 class operators():
-    def __init__(self, grad_func, alpha1=0.8, alpha2=0.99, N=50) -> None:
+    def __init__(self, grad_func, alpha1=0.8, alpha2=0.99, N=10) -> None:
         self.grad_func = grad_func
         self.alpha1 = alpha1
         self.alpha2 = alpha2
@@ -15,19 +15,19 @@ class operators():
 
 
     def fractional(self, p, pm_1, lr):
-        return (1 / torch.exp(torch.lgamma(torch.tensor(2 - self.alpha1)))) * pm_1.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - self.alpha1)
+        return (1 / torch.exp(torch.lgamma(torch.tensor(2 - self.alpha1)))) * p.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - self.alpha1)
     
     
     def multi_fractional(self, p, pm_1, lr):
-        t1 = (1 / torch.exp(torch.lgamma(torch.tensor(2 - self.alpha1)))) * pm_1.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - self.alpha1)
-        t2 = (1 / torch.exp(torch.lgamma(torch.tensor(2 - self.alpha2)))) * pm_1.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - self.alpha2)
+        t1 = (1 / torch.exp(torch.lgamma(torch.tensor(2 - self.alpha1)))) * p.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - self.alpha1)
+        t2 = (1 / torch.exp(torch.lgamma(torch.tensor(2 - self.alpha2)))) * p.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - self.alpha2)
         
         return 0.5 * t1 + 0.5 * t2
         
 
     def distributed_fractional(self, p, pm_1, lr):
         d_alpha = (self.alpha2 - self.alpha1) / self.N
-        integral = lambda alpha: ((2 * (alpha - self.alpha1)) / (torch.exp(torch.lgamma(torch.tensor(2 - alpha))) * (self.alpha2 - self.alpha1) ** 2)) * pm_1.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - alpha)
+        integral = lambda alpha: ((2 * (alpha - self.alpha1)) / (torch.exp(torch.lgamma(torch.tensor(2 - alpha))) * (self.alpha2 - self.alpha1) ** 2)) * p.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - alpha)
         
         delta = 0.5 * integral(self.alpha1)
         for n in range(1, self.N):
