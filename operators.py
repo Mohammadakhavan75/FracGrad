0e2a30de9e4523f89ad0886876673b0031ca1aac
asalmohammadjafari <1382asal@gmail.com>
2024-08-15 10:36:17 +0330

    testing mnist and cfar10, need to debugg

--- operators.py
+++ operators.py
@@ -1,31 +1,33 @@
 from scipy.special import gamma
 import numpy as np
+import torch
 
 class operators():
-    def __init__(self, grad_func, alpha1=0.9, alpha2=1.1, N=50) -> None:
+    def __init__(self, grad_func, alpha1=0.8, alpha2=0.99, N=50) -> None:
         self.grad_func = grad_func
         self.alpha1 = alpha1
         self.alpha2 = alpha2
         self.N = N
         
     def integer(self, p, pm_1, lr):
-        return self.grad_func(p)
+        # return self.grad_func(p)
+        return p.grad
 
 
     def fractional(self, p, pm_1, lr):
-        return pm_1 - (lr / gamma(2 - self.alpha1)) * self.grad_func(p) * np.abs(pm_1 - p) ** (1 - self.alpha1)
-
-
+        return (1 / torch.exp(torch.lgamma(torch.tensor(2 - self.alpha1)))) * pm_1.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - self.alpha1)
+    
+    
     def multi_fractional(self, p, pm_1, lr):
-        t1 = (1/gamma(2 - self.alpha1)) * self.grad_func(p) * np.abs(pm_1 - p) ** (1 - self.alpha1)
-        t2 = (1/gamma(2 - self.alpha2)) * self.grad_func(p) * np.abs(pm_1 - p) ** (1 - self.alpha2)
-
+        t1 = (1 / torch.exp(torch.lgamma(torch.tensor(2 - self.alpha1)))) * pm_1.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - self.alpha1)
+        t2 = (1 / torch.exp(torch.lgamma(torch.tensor(2 - self.alpha2)))) * pm_1.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - self.alpha2)
+        
         return 0.5 * t1 + 0.5 * t2
-
+        
 
     def distributed_fractional(self, p, pm_1, lr):
         d_alpha = (self.alpha2 - self.alpha1) / self.N
-        integral = lambda alpha: ((2 * (alpha - self.alpha1)) / (gamma(2 - alpha) * (self.alpha2 - self.alpha1) ** 2)) * self.grad_func(p) * np.abs(pm_1 - p) ** (1 - alpha)
+        integral = lambda alpha: ((2 * (alpha - self.alpha1)) / (torch.exp(torch.lgamma(torch.tensor(2 - alpha))) * (self.alpha2 - self.alpha1) ** 2)) * pm_1.grad.detach() * torch.abs(p.data.detach() - pm_1.data.detach()) ** (1 - alpha)
         
         delta = 0.5 * integral(self.alpha1)
         for n in range(1, self.N):
@@ -33,4 +35,4 @@ class operators():
         
         delta += 0.5 * integral(self.alpha2)
 
-        return delta
+        return delta / 50
